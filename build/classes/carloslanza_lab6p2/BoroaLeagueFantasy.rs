613351afe58c8b0b1cc2e94f0e54b6cf3f76bb61
Carloslanza <131491059+Carloslanza@users.noreply.github.com>
2024-02-23 18:52:36 -0600

    Add files via upload
    modificar y eliminar jugadores de la lista hecho

---
+++ build/classes/carloslanza_lab6p2/BoroaLeagueFantasy.rs
@@ -0,0 +1,6 @@
+com.mycompany.carloslanza_lab6p2.BoroaLeagueFantasy$1
+com.mycompany.carloslanza_lab6p2.BoroaLeagueFantasy$4
+com.mycompany.carloslanza_lab6p2.BoroaLeagueFantasy$5
+com.mycompany.carloslanza_lab6p2.BoroaLeagueFantasy$2
+com.mycompany.carloslanza_lab6p2.BoroaLeagueFantasy$3
+com.mycompany.carloslanza_lab6p2.BoroaLeagueFantasy
---
+++ src/carloslanza_lab6p2/Equipo.java
@@ -0,0 +1,49 @@
+package carloslanza_lab6p2;
+
+import java.util.ArrayList;
+
+public class Equipo {
+    private String pais;
+    private String nombre;
+    private String ciudad;
+    private String estadio;
+    private ArrayList<Jugador> jugadores = new ArrayList<>();
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" + "pais=" + pais + ", nombre=" + nombre + ", ciudad=" + ciudad + ", estadio=" + estadio + '}';
+    }
+    
+}
